1b37e8830b8bab2aabb6890e6d0d2c71cbb8b75e
Warren MacEvoy <wmacevoy@gmail.com>
2022-02-12 18:33:39 -0700

    endline

--- docs/cards.py
+++ docs/cards.py
@@ -1,17 +1,19 @@
+#!/usr/bin/env python3
+
 UPS=[('A','ALPHA'),  ('B','BRAVO'), ('C','CHARLIE'),('D','DELTA'), ('E','ECHO'),('F','FOXTROT'),('G','GOLF'),('H','HOTEL'),('I','INDIA'),('J','JULIETT'),
      ('K','KILO'),    ('L','LIMA'),  ('M','MIKE'),   ('N','NOVEMBER'), ('O','OSCAR'), ('P','PAPA'), ('Q','QUEBEC'),('R','ROMEO'),('S','SIERRA'),('T','TANGO'),
      ('U','UNIFORM'),('V','VICTOR'),('W','WHISKEY'),('X','XRAY'),('Y','YANKEE'),('Z','ZULU'),('{','{curly'),('}','curly}'),('[','[bracket'),(']','bracket]'),
-     ('_','under'),  (':','colon'), ('!','bang!'),   ('&#96;','&#96;back&#96;'),('heart-broken',''),('heart-full',''),('','↓aA'),('',''),('','⇊ab⇈AB'),('','')]
+     ('_','under_bar'),  (':','colon:'), ('!','bang!'),   ('&#96;','&#96;back&#96;'),('heart-broken',''),('heart-full',''),('','↓aA'),('',''),('','⇊ab⇈AB'),('','')]
 
 DOWNS=[('0','zero'),('1','one'),('2','two'),('3','three'),('4','four'),('5','five'),('6','six'),('7','seven'),('8','eight'),('9','nine'),
        ('A','ALPHA'),('B','BRAVO'),('C','CHARLIE'),('D','DELTA'),('E','ECHO'),('F','FOXTROT'),('@','at'),('=','equal'),('\\','backslash'),('~','tilde'),
        ('#','pound'),('$','dollar'),('%','percent'),('^','carat'),('&amp;','and'),('|','pipe'),('-','dash'),('+','plus'),('/','slash'),('*','astrisk*'),
-       ('↩','newline'),(';','semicolon'),('?','question'),('&#39;','&#39;single&#39;'),('sad',''),('happy',''),('',''),('','↑a1'),('',''),('','⇈ab⇊12')]
+       ('↩',"\"<tspan dy=\"-4\" x=\"0\">end↩</tspan><tspan x=\"0\" dy=\"3\">line↩</tspan>"),(';','semicolon;'),('?','question?'),('&#39;','&#39;single&#39;'),('sad',''),('happy',''),('',''),('','↑a1'),('',''),('','⇈ab⇊12')]
 
 PLAINS=[('a','alpha'),('b','bravo'),('c','charlie'),('d','delta'),('e','echo'),('f','foxtrot'),('g','golf'),('h','hotel'),('i','india'),('j','juliett'),
         ('k','kilo'),('l','lima'),('m','mike'),('n','november'),('o','oscar'),('p','papa'),('q','quebec'),('r','romeo'),('s','sierra'),('t','tango'),
         ('u','uniform'),('v','victor'),('w','whiskey'),('x','xray'),('y','yankee'),('z','zulu'),('&lt;','less'),('&gt;','more'),('(','(paren'),(')','paren)'),
-        ('␣','space'),(',','comma'),('.','period'),('&#34;','&#34;double&#34;'),('thumb-down',''),('thumb-up',''),('','↓1a'),('','↑Aa'),('','⇊12⇈ab'),('','⇈AB⇊ab')]
+        ('␣','s p a c e'),(',','comma,'),('.','period.'),('&#34;','&#34;double&#34;'),('thumb-down',''),('thumb-up',''),('','↓1a'),('','↑Aa'),('','⇊12⇈ab'),('','⇈AB⇊ab')]
 
 SUITS=['suit-club','suit-diamond','suit-heart','suit-spade']
 ARROWS=['↓','↑','⇊','⇈']
@@ -19,15 +21,22 @@ ARROWS=['↓','↑','⇊','⇈']
 with open("card-template.svg") as f:
     svg = f.read()
 
+def spans(str):
+    lines = str.split("\n")
+    if len(lines) == 1:
+        return lines[0]
+    else:
+        return "<tspan>"+("</tspan><tspan>".join(str.split("\n"))) + "</tspan>"
+    
 def card(number):
     suit = number // 10
     face = number % 10
     up = UPS[number][0]
-    hint_up = UPS[number][1]
+    hint_up = spans(UPS[number][1])
     down = DOWNS[number][0]
-    hint_down = DOWNS[number][1]
+    hint_down = spans(DOWNS[number][1])
     plain = PLAINS[number][0]
-    hint_plain = PLAINS[number][1]
+    hint_plain = spans(PLAINS[number][1])
     translate_type = "translate-text" if number != 34 and number != 35 else "translate-pics"
     formats="formats" if number < 36 else "formats-dark"
 
@@ -51,5 +60,3 @@ for number in range(40):
     cardsvg = card(number)
     with open(str(number//10) + str(number%10) + ".svg","w") as f:
         f.write(cardsvg)
-
-
